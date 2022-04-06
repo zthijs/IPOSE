ce645177ebfe26515048941904f7db222bac3bd4
nassirakhales <nassirakhales@gmail.com>
2022-04-06 13:31:59 +0200

    path 2

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,26 +2,22 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.pathfinding.CellMoveComponent;
 import com.almasb.fxgl.pathfinding.CellState;
 import com.almasb.fxgl.pathfinding.astar.AStarGrid;
-import com.almasb.fxgl.pathfinding.astar.AStarMoveComponent;
 import com.almasb.fxgl.physics.CollisionHandler;
-import com.almasb.fxgl.texture.Texture;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 import java.util.Map;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
 
 public class Game extends GameApplication {
-    private int [][] mudPaths = {{0, 80}, {80,80},{160, 80}, {160, 160}, {160, 240},{240, 240},{320,240},{320, 320},{320, 400}, {320,480}, {400,480}, {480,480},{560, 480},{640, 480},{720, 480},{800, 480},{880, 480},{960, 480},{1040, 480}};
-    private int [][] mudPaths2 = { {80,80},{160, 80}, {160, 160}, {160, 240}};
+
+    public final int [][] PATH_1 = {{0, 80}, {80,80},{160, 80}, {160, 160}, {160, 240},{240, 240},{320,240},{320, 320},{320, 400}, {320,480}, {400,480}, {480,480},{560, 480},{640, 480},{720, 480},{800, 480},{880, 480},{960, 480},{1040, 480}};
+    public final int [][] TOWERS_1 = {{80, 160},{480, 400},{880, 560}};
+
+    public final int [][] PATH_2 = { {80,80},{160, 80}, {160, 160}, {160, 240}};
+
     public static void main (String[] args) {
         launch(args);
     }
@@ -48,6 +44,12 @@ public class Game extends GameApplication {
 
         entityBuilder().view("stone.jpg").zIndex(200).at(1000,0).scale(2, 2).buildAndAttach();
 
+        for (int[] cords: TOWERS_1) {
+            entityBuilder().view("platform.png").zIndex(210).at(cords[0],cords[1]).onClick(v->{
+                v.setVisible(!v.isVisible());
+            }).buildAndAttach();
+        }
+
 
         var grid = new AStarGrid(1280/80,720/80);
         int cellWidth = 80;
@@ -56,98 +58,60 @@ public class Game extends GameApplication {
 
         Enemy nee = new Enemy(60, 100, 60, "AIspeed", grid);
 
-        Tower tower = new Tower(100, 100);
-
-        grid.forEach(huts -> {
-            huts.setState(CellState.NOT_WALKABLE);
+        grid.forEach(v -> {
+            v.setState(CellState.NOT_WALKABLE);
         });
 
-        for (int[] cords : mudPaths) {
+        for (int[] cords : PATH_1) {
             genMudPiece(cords[0],cords[1]);
             grid.get(cords[0]/cellWidth,cords[1]/cellHeight).setState(CellState.WALKABLE);
         }
 
-        nee.walk(mudPaths[mudPaths.length -1][0],mudPaths[mudPaths.length - 1][1]);
+        nee.walk(PATH_1[PATH_1.length -1][0],PATH_1[PATH_1.length - 1][1]);
 
-
-
-        
     }
 
 
-
+    // Plaats de modderpaden op de grid.
     private void genMudPiece(int x, int y){
-
-        entityBuilder()
-                .view("mud.png")
-                .zIndex(11).at(x,y)
-                .onClick(f->{
-
-                })
-                .buildAndAttach();
+        entityBuilder().view("mud.png").zIndex(11).at(x,y).buildAndAttach();
     }
 
-
-    //fix dit, moet EntityType & Entity opgeven, moet var van klasse aanpassen
     @Override
     protected void initPhysics(){
         FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.BULLET, EntityTypes.ENEMY) {
             @Override
             protected void onCollision(Entity bullet, Entity enemy) {
                 bullet.removeFromWorld();
-
-                //bullet.getDamage bestaat nog niet, tijdelijk gecomment hieronder
-                //enemy.setHealth(enemy.getHealth() - bullet.getDamage());
-
-                //if(enemy.getHealth() <= 0) {
-                //    enemy.remove();
-                //}
-
-
-
+                //line hieronder wil je schade toepassen bij de enemy entity
+                //if statement -> enemy.removeFromWorld() als enemy.health <= 0?
                 FXGL.inc("score", +1);
-
             }
         });
     }
 
-    private void makeLabel(String labelContents, int Xcoord, int Ycoord, boolean var){
-        Label label = new Label(labelContents);
-        label.setStyle(
-                "-fx-font-family: 'Comic Sans MS'; " +
-                "-fx-font-size: 20; " +
-                "-fx-text-fill: white;");
-        label.setTranslateX(Xcoord);
-        label.setTranslateY(Ycoord);
-        FXGL.getGameScene().addUINode(label);
-        if (var == true) {
-            label.textProperty().bind(FXGL.getWorldProperties().intProperty(labelContents).asString());
-        }
-    }
-
     @Override
     protected void initUI(){
-        makeLabel("Score: ",1016,12,false);
-        makeLabel("Health: ",1016,44,false);
-        makeLabel("Money: ",1016,76,false);
-        makeLabel("score",1090,12,true);
-        makeLabel("health",1090,44,true);
-        makeLabel("money",1090,76,true);
+        //method voor het maken van tekstelementen binnen UI -> DRY
+        Label scoreText = new Label("Score: ");
+        scoreText.setTranslateX(5);
+        //scoreText.setTranslateY(0);
+
+        Label score = new Label();
+        score.setTranslateX(40);
+        //score.setTranslateY(0);
+
+        score.textProperty().bind(FXGL.getWorldProperties().intProperty("score").asString());
+
+        FXGL.getGameScene().addUINode(score);
+        FXGL.getGameScene().addUINode(scoreText);
     }
 
     @Override
     protected void initGameVars(Map<String, Object> vars){
         vars.put("score",0);
-        vars.put("health",20);
-        vars.put("money",100);
     }
 
-    //key input
-    @Override
-    protected void initInput() {
-        FXGL.onKey(KeyCode.F5, () -> {
-        });
-    }
+
 }
-
 
